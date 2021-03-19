66209777b2d8303e527812d8129f9ba6448aa35b
Wizard <1160799@alumno.um.edu.mx>
2021-03-18 19:49:07 -0600

    Funcion de encriptar contrasena lista

--- models/registrousuario.js
+++ models/registrousuario.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const UsuarioSchema = mongoose.Schema({
     nombre: {
@@ -36,5 +37,27 @@ const UsuarioSchema = mongoose.Schema({
     }
 
 })
+
+UsuarioSchema.pre('save', function (next){
+    bcrypt.genSalt(10).then(salts => {
+        bcrypt.hash(this.contrasena, salts).then(hash => {
+            this.contrasena = hash;
+            next();
+        }).catch(error => next(error))
+    }).catch(error => next(error))
+});
+
+//Metodo para comparar contrasenas
+/*
+UsuarioSchema.methods.compararClave =  function (contrasena, cb){
+    bcrypt.compare(contrasena, this.password, function (err, resultado){
+        if (err){
+            return cb(err)
+        }
+        cb(null, resultado)
+    })
+}
+
+ */
 
 module.exports = mongoose.model('registrousuario', UsuarioSchema)
